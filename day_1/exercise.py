3fecc266b0f00c80db8eaa50e5b13e2ebf005c71
J C <jessy1082@yahoo.com>
2021-09-13 22:18:37 -0400

    wk4day1json

--- day_1/exercise.py
+++ day_1/exercise.py
@@ -1,3 +1,41 @@
 # ITP Week 4 Day 1 Exercise
 
 # https://data.messari.io/api/v2/assets
+
+import requests
+import json
+import openpyxl
+
+#wb = openpyxl.load_workbook("C:/Users/jessy/Desktop/Vetstech_Python/itp_week_4/day_1/output.xlsx")
+#sheet = wb['sheet1']
+
+def get_data(url):
+    response = requests.get(url)
+    json_result = response.text
+    #print(response)
+    clean_data = json.loads(json_result)
+    result = clean_data["data"]
+    return result
+
+#row = 1 
+
+data = get_data ("https://data.messari.io/api/v2/assets")
+symbols = [] 
+print(data) # data comes out
+
+#for i in data:
+
+# def write_data(result):
+#       global row
+#       for character in result:
+#          symbols.append(character['symbol'])
+
+# print(symbols)
+
+
+
+#         sheet['A' + str(row)] = character['id']
+#         sheet['B' + str(row)] = character['symbol']
+#         sheet['C' + str(row)] = character['name']
+#         sheet['D' + str(row)] = character['slug']
+#         row+=1
--- day_1/lecture.py
+++ day_1/lecture.py
@@ -1,1 +1,49 @@
 # ITP Week 4 Day 1 Lecture
+# https://rickandmortyapi.com/api/character
+
+## GET THE DATE
+## CLEAN THE DATA
+## LOOP THROUGH DATA
+## OUTPUT THE DATA
+# ITP Week 4 Day 1 Lecture
+
+import requests
+import json
+import openpyxl
+
+wb = openpyxl.load_workbook("/Users/jessy/Desktop/Vetstech_Python/itp_week_4/day_1/output.xlsx")
+sheet = wb["Sheet1"]
+
+def get_data(url):
+    response = requests.get(url)
+    # print(response)
+    json_result = response.text
+    # print(json_result)
+    # print(type(json_result))
+    clean_data = json.loads(json_result)
+    result = clean_data["results"]
+    return result
+
+row = 1
+
+def write_data(result):
+    global row
+    for character in result:
+        sheet['A' + str(row)] = character['name']
+        sheet['B' + str(row)] = character['species']
+        sheet['C' + str(row)] = character['gender']
+        sheet['D' + str(row)] = character['location']['name']
+        row+=1
+
+
+result_1 = get_data("https://rickandmortyapi.com/api/character")
+result_2 = get_data("https://rickandmortyapi.com/api/character/?page=2")
+result_3 = get_data("https://rickandmortyapi.com/api/character/?page=3")
+
+write_data(result_1)
+write_data(result_2)
+write_data(result_3)
+write_data(get_data("https://rickandmortyapi.com/api/character/?page=4"))
+
+
+wb.save("/Users/jessy/Desktop/Vetstech_Python/itp_week_4/day_1/output.xlsx")
